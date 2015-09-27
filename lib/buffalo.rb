0803dd83e10d6f705200f59e5e1753340d36af1f
Elia Schito <elia@schito.me>
2015-09-28 00:19:35 +0200

    Stuff

--- lib/buffalo.rb
+++ lib/buffalo.rb
@@ -1,3 +1,8 @@
+# coding: utf-8
+
+# Encoding.default_external = Encoding::UTF_8
+# Encoding.default_internal = Encoding::UTF_8
+
 require 'buffalo/version'
 require 'buffalo/bill'
 
--- lib/buffalo/bill.rb
+++ lib/buffalo/bill.rb
@@ -1,6 +1,7 @@
 require 'ostruct'
 require 'tilt'
 require 'pdfkit'
+require 'yaml'
 
 module Buffalo
   class Bill
@@ -10,21 +11,24 @@ module Buffalo
 
     attr_reader :dir
 
-    def load_info type=nil, id
-      hash = YAML.load_file(File.expand_path("#{dir}/#{type}/#{id}.yml"))
+    def load_info type, id=nil
+      path = [dir, type, id].compact.join('/')
+      hash = YAML.load_file(File.expand_path("#{path}.yml"))
       hash['id'] = id
       OpenStruct.new hash
     end
 
     def make_all_invoices
       Dir[File.expand_path("#{dir}/invoices/*.yml")].each { |i| make_invoice i }
+      # system 'open', File.dirname(html_file)
     end
 
     def make_invoice invoice_file
+      log("Generating invoice from: #{File.basename invoice_file}")
       invoice_id = File.basename invoice_file, '.yml'
       invoice        = load_info(:invoices, invoice_id)
       invoice.id     = invoice_id
-      invoice.me     = load_info(:me)
+      invoice.me     = load_info(:me, invoice.me)
       invoice.client = load_info(:clients, invoice.client)
 
       invoice.items = invoice.items.map do |item|
@@ -32,22 +36,35 @@ module Buffalo
       end
 
       template = Tilt.new("./templates/#{invoice.template}.haml")
-      html = template.render(invoice)
-      pdf  = PDFKit.new(html, :page_size => 'A4').to_pdf
+      output_base = "./output/#{invoice.me.id}-#{invoice.id}-#{invoice.client.id}"
 
-      html_file = "./output/#{invoice.id}-#{invoice.client.id}.html"
-      pdf_file  = "./output/#{invoice.id}-#{invoice.client.id}.pdf"
+      html = nil # keep it for the pdf
 
-      File.open(html_file, 'w') {|f| f << html}
-      File.open(pdf_file,  'w') {|f| f << pdf }
+      render_file output_base, :html do
+        html = template.render(invoice)
+      end
 
-      system 'open', File.dirname(html_file)
+      # render_file output_base, :pdf do
+      #   PDFKit.new(html, :page_size => 'A4').to_pdf
+      # end
+    end
+
+    def render_file base, type
+      log("Generating #{type}...")
+      contents = yield
+      log("Writing #{type}...")
+      file_name = "#{base}.#{type}"
+      File.open(file_name, 'w') {|f| f << contents}
     end
 
     # def watch!
     #   require 'watchr'
     #   watch("#{dir}")
     # end
+
+    def log message
+      puts message
+    end
   end
 end
 
